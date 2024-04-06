010386c611e71f4e75a19864dd004e76ca8cbfe2
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-04-05 21:52:20 -0300

    Refatorando aplicação com ApolloClient

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { Rotas } from './routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ABApolloClient from './Components/ABApolloClient';
 
 const queryClient = new QueryClient();
 
 function App() {
-
   return (
-    <>
+    <ABApolloClient>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Rotas />
         </BrowserRouter>
       </QueryClientProvider>
-    </>
+    </ABApolloClient>
   )
 }
 

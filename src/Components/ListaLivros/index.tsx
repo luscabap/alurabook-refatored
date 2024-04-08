b5334a8574bb6c90b6da317a963b219f2db71010
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-04-08 13:02:49 -0300

    Componente ListaLivros: Consulta com GraphQL e refetch para buscar por titulo

--- src/Components/ListaLivros/index.tsx
+++ src/Components/ListaLivros/index.tsx
@@ -1,21 +1,67 @@
-import { useQuery } from '@tanstack/react-query';
+import { gql, useQuery } from '@apollo/client';
 import { ICategoriaProps } from '../../interface/ICategoriaProps';
-import * as Style from './style';
-import { obterLivrosDaCategoria } from '../../http';
 import { MiniCard } from '../MiniCard';
+import * as Style from './style';
+import { ILivroProps } from '../../interface/ILivroProps';
+import { AbBotao, AbCampoTexto } from 'ds-alurabooks';
+import { useState } from 'react';
 
 interface IListaLivrosProps {
     categoria: ICategoriaProps
 }
 
+const OBTER_LIVROS = gql`
+    query ObterLivros($categoriaId: Int, $titulo: String) {
+        livros (categoriaId: $categoriaId, titulo: $titulo) {
+            id,
+            slug,
+            titulo,
+            imagemCapa,
+            opcoesCompra {
+                id
+                preco
+            }
+        }
+    }
+
+`
+
 export const ListaLivros = ({ categoria }: IListaLivrosProps) => {
-    const { data: produtos } = useQuery(['buscaLivrosPorCategoria', categoria], () => obterLivrosDaCategoria(categoria))
+    const { data, refetch } = useQuery<{ livros: ILivroProps }>(OBTER_LIVROS, {
+        variables: {
+            categoriaId: categoria.id
+        }
+    })
+
+    const [textoBusca, setTextoBusca] = useState('')
+
+    const buscarLivros = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault();
+        if (textoBusca) {
+            refetch({
+                categoriaId: categoria.id,
+                titulo: textoBusca
+            })
+        }
+
+        setTextoBusca("");
+    }
 
     return (
-        <Style.ContainerListaLivros>
+        <Style.ContainerPagina>
+            <form className='container__busca' onSubmit={buscarLivros}>
+                <AbCampoTexto 
+                    placeholder='Digite o título do livro'
+                    value={textoBusca}
+                    onChange={setTextoBusca}
+                />
+                <AbBotao texto='Buscar'/>
+            </form>
+            <Style.ContainerListaLivros>    
             {
-                produtos?.map(livro => <MiniCard livro={livro} categoria={categoria} key={livro.id} />)
+                data?.livros.map(livro => <MiniCard livro={livro} categoria={categoria} key={livro.id} />)
             }
-        </Style.ContainerListaLivros>
+            </Style.ContainerListaLivros>
+        </Style.ContainerPagina>
     )
 }
--- src/Components/ListaLivros/style.ts
+++ src/Components/ListaLivros/style.ts
@@ -1,8 +1,31 @@
 import styled from "styled-components";
 
+export const ContainerPagina = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: space-around;
+    justify-content: center;
+
+        .container__busca{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        
+        input {
+            width: 800px;
+            text-align: center;
+            border-radius: 20px;
+            padding: 1rem;
+            border: 1px solid #0F0F0F
+        }
+    }
+`
+
 export const ContainerListaLivros = styled.div`
     display: flex;
-    justify-content: space-around;
+    align-items: space-around;
     gap: 2rem;
     flex-wrap: wrap;
     margin: 24px auto;
